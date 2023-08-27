e46d2eb17d2f9c890af1a619c68d054eb96632e8
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-27 19:29:17 +0000

    que_stack.c

---
+++ que_stack.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+char *args[2];
+
+/**
+ * stacks - function that switch to default LIFO
+ * @stack: double pointer to the head of the stack
+ * @line_number: the index of the current line we ar working on
+ * Return: void
+ */
+void stacks(stack_t **stack _attribute_((unused)), unsigned int line_number)
+{
+	(void) line_number;
+
+	args[1] = "stack";
+}
+
+/**
+ * queue - function that switch to default FIFO
+ * @stack: double pointer to the head of the stack
+ * @line_number: the index of the current line we ar working on
+ * Return: void
+ */
+void queue(stack_t **stack _attribute_((unused)), unsigned int line_number)
+{
+	(void) line_number;
+
+	args[1] = "queue";
+}
